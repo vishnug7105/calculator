9158cde9fbc6aa0571afa9e2bd2d1c72ddc4772f
vishnug7105 <vishnug7105@gmail.com>
2023-09-23 11:24:51 +0530

    modification

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+const number = document.querySelectorAll("#numbers");
+const display = document.querySelector("#dailog-box");
+const operation = document.querySelectorAll("#operations");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
+for (let i = 0; i < number.length; i++){
+    number[i].style.cursor = "pointer";
+    number[i].addEventListener("click", e => {
+        display.innerHTML = display.innerHTML + number[i].innerText;
 
     })
-})
+
+}
+
+for (let i = 0; i < operation.length; i++) {
+    operation[i].style.cursor = "pointer";
+
+    operation[i].addEventListener("click", e => {
+        switch (operation[i].innerText) {
+            case "DEL":
+                display.innerText = "";
+                break;
+            case "C":
+                display.innerText = display.innerHTML.slice(0, -1);
+                break;
+            case "=":
+                if (display.innerHTML.length == 0) {
+                    display.innerHTML = "ENTER NUMBER";
+                    break;
+                }
+                else {
+                    try {
+                        display.innerText = eval(display.innerHTML);
+                    break;
+                    }
+                    catch (e) {
+                        display.innerText = "Enter vaild way";
+                        break;
+                    }
+                    break;
+                }
+
+        }
+    })
+}
